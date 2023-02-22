c0d3246bcd55276e86f1f0d095fa7492cf4ce1bd
Naresh <mail2.nareshc@gmail.com>
2023-02-22 14:38:12 +0530

    adding one

--- Creation2.java
+++ Creation2.java
@@ -1,6 +1,6 @@
 
 public class Creation2{
 public static void main(String [ ] args) {
-           System.out.println("Bombay to pune one ");
+           System.out.println("Bombay to pune 222 ");
     }
 }
