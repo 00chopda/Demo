41864f337b5cd3d9913777cd028cc560239428fb
Naresh <mail2.nareshc@gmail.com>
2023-02-22 13:37:41 +0530

    adding .class file

--- Job1.java
+++ Job1.java
@@ -1,6 +1,6 @@
 
 public class Job1{
 public static void main(String [ ] args) {
-           System.out.println("Hi Dear1 ");
+           System.out.println("Bombay to pune ");
     }
 }
--- Job2.java
+++ Job2.java
@@ -1,6 +1,6 @@
 
 public class Job2{
 public static void main(String [ ] args) {
-           System.out.println("Hi Dear Rajesh ");
+           System.out.println("pune to nashik ");
     }
 }
